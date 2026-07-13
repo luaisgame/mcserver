52460617e5a5e92ab123744a1389670f8bb46e71
luaisgame <luaisgame@gmail.com>
2026-07-14 01:10:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,9 +9,9 @@ PLAYIT_PID=$!
 
 echo "Starting Minecraft..."
 java \
-  -Xms"${MIN_RAM:-2G}" \
-  -Xmx"${MAX_RAM:-4G}" \
-  -jar fabric-server-launch.jar \
+  -Xms"${MIN_RAM:-32G}" \
+  -Xmx"${MAX_RAM:-12G}" \
+  -jar fabric-server.jar \
   nogui &
 MC_PID=$!
 
