e06352d343f45495d486717df3ac3b3110ecb2a0
luaisgame <luaisgame@gmail.com>
2026-07-14 00:11:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,7 @@
 #!/usr/bin/env bash
-set -e
 
-cd /data
+echo "Temporary placeholder service running."
 
-echo "Starting Playit..."
-playit --secret "$SECRET_KEY" 2>&1 | tee /data/playit.log &
-PLAYIT_PID=$!
-
-echo "Starting Fabric Minecraft server..."
-java \
-  -Xms"${MIN_RAM:-2G}" \
-  -Xmx"${MAX_RAM:-4G}" \
-  -jar fabric-server.jar \
-  nogui &
-MINECRAFT_PID=$!
-
-cleanup() {
-    echo "Stopping services..."
-    kill "$PLAYIT_PID" "$MINECRAFT_PID" 2>/dev/null || true
-    wait || true
-}
-
-trap cleanup SIGTERM SIGINT
-
-wait -n "$PLAYIT_PID" "$MINECRAFT_PID"
+while true; do
+    sleep 3600
+done
